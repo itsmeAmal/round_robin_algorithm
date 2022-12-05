0c8ea629352cd3b490a0c87d9790cd7f1181ded9
Amal <wickramarathna.amal@gmail.com>
2022-12-05 05:42:57 +0530

    Added arrival time looping and get all arrival times from data.txt file

--- read_data_each_line.sh
+++ read_data_each_line.sh
@@ -1,26 +1,55 @@
 #!/bin/bash 
-
- file='data_file2.txt'
+ file='data1.txt'
  arrProcessLabels=()
  arrProcessLabels+=("Time")
+ arrayArrivalTime=()
 
- i=1
+ initialTime=0
+
+ i=0
  echo "----- Data file ------ "
-
  while read line; do
  echo "$line" |tee -a write-3.txt
 
  declare -a arrLineValues=()
  arrLineValues+=($line)
- 
+ #get first index of each line (as process label)
  arrProcessLabels+=($arrLineValues)
+
+ #get third index of each line (as time)
+ arrayBreakLineInto=()
+ arrayBreakLineInto+=($line)
+ arrayArrivalTime+=(${arrayBreakLineInto[2]})
+
+ #increase i by one value
  i=$((i+1))
 
  done < $file
  
  echo ""
  echo "---- Time and process labels ----"
- echo ${arrProcessLabels[@]} 
+ echo ${arrProcessLabels[@]}  
+
+#  echo ${arrayArrivalTime[@]}
+
+ while [ $initialTime -le ${#arrayArrivalTime[@]} ]
+ do 
+   echo "$initialTime " 
+   initialTime=$(($initialTime+1))
+ done
+
+
+
+
+
+
+
+
+
+
+
+ 
+
 
 
 
